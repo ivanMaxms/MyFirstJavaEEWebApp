3fc769c41d894a62c170c2085159dc473e08febe
Ivan Andreev <van079@mail.ru>
2018-09-21 10:26:57 +0300

    added redirect from AddServlet to SqServlet

--- src/com/andreev/AddServlet.java
+++ src/com/andreev/AddServlet.java
@@ -1,5 +1,7 @@
 package com.andreev;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,14 +9,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class AddServlet extends HttpServlet {
-    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
         int i = Integer.parseInt(req.getParameter("num1"));
         int j = Integer.parseInt(req.getParameter("num2"));
 
         int k = i+j;
 
-        PrintWriter out = res.getWriter();
-        out.println("result is " + k);
+        RequestDispatcher rd = req.getRequestDispatcher("/sq");
+        rd.forward(req, res);
 
     }
 
--- src/com/andreev/SqServlet.java
+++ src/com/andreev/SqServlet.java
@@ -1,6 +1,16 @@
 package com.andreev;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class SqServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
+        PrintWriter out = res.getWriter();
+        out.println("Request from ADD was accepted");
+    }
 }
